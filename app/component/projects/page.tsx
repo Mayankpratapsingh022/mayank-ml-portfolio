a6503235cb9bdc3c407ac310d120ce2c19bdf557
Mayank Pratap Singh <mayankpratapsingh022@gmail.com>
2024-09-28 21:09:16 +0530

    added description

--- app/component/projects/page.tsx
+++ app/component/projects/page.tsx
@@ -6,12 +6,20 @@ const CardGrid: React.FC = () => {
   // Dummy data for multiple cards
   const cardData = [
     {
+      title: "Customer Satisfaction Prediction using ZenML",
+      paragraph: "Predicted customer satisfaction scores for future orders using historical e-commerce data from the Brazilian E-Commerce Public Dataset by Olist. This project leverages multiple machine learning models like CatBoost, XGBoost, and LightGBM, built within a ZenML pipeline to create a production-ready solution.",
+      techStack: [ "XGBoost", "Optuna", "ZenML", "Streamlit", "MLflow"],
+      viewUrl: "",
+      githubUrl: "",
+      imageSrc: "/customer_pipeline.jpg", 
+    },
+    {
       title: "Machine Learning Algorithms From Scratch",
       paragraph: "Implemented key machine learning algorithms from scratch using Python & Numpy. This project provides a deeper understanding of algorithmic internals without relying on libraries like scikit-learn or TensorFlow.",
       techStack: ["Linear Regression", "Logistic Regression", "Support Vector Machine (SVM)", "Naive Bayes", "K-Means", "PCA"],
       viewUrl: "https://github.com/Mayankpratapsingh022/ML_from_Scratch",
       githubUrl: "https://github.com/Mayankpratapsingh022/ML_from_Scratch",
-      imageSrc: "/Ml_from_S.jpg", // Add imageSrc here
+      imageSrc: "/Ml_from_S.png", // Add imageSrc here
     },
     {
       title: "Neural Network From Scratch",
@@ -22,14 +30,7 @@ const CardGrid: React.FC = () => {
       imageSrc: "/neutral_net.jpg", // Add imageSrc here
     },    
 
-    {
-      title: "Customer Satisfaction Prediction using ZenML",
-      paragraph: "Predicted customer satisfaction scores for future orders using historical e-commerce data from the Brazilian E-Commerce Public Dataset by Olist. This project leverages multiple machine learning models like CatBoost, XGBoost, and LightGBM, built within a ZenML pipeline to create a production-ready solution.",
-      techStack: [ "XGBoost", "Optuna", "ZenML", "Streamlit", "MLflow"],
-      viewUrl: "",
-      githubUrl: "",
-      imageSrc: "/customer_pipeline.jpg", 
-    }
+
 
   ];
 
